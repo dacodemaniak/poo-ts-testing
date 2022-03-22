d573c79bc664abd719daa4a1f08e65bf3dee711c
DaCodeManiaK <jla.webprojet@gmail.com>
2022-03-22 16:50:07 +0100

    Test getter and setter

--- test/room.model.spec.ts
+++ test/room.model.spec.ts
@@ -10,5 +10,15 @@ describe('Testing room.model.ts', () => {
     test(`Room must have a 'roomNumber' attribute`, () => {
         const hasProperty: boolean = room.hasOwnProperty('roomNumber');
         expect(hasProperty).toBeTruthy();
+    });
+
+    test(`Room number setting must return the current RoomModel instance`, () => {
+        const afterSetter: RoomModel = room.setRoomNumber(10);
+        expect(afterSetter).toStrictEqual(room);
+    });
+
+    test(`Room number must be '10' after setting`, () => {
+        room.setRoomNumber(10);
+        expect(room.getRoomNumber()).toBe(10);
     })
 });

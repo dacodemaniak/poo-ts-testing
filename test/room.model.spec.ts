8ea3ef9995aa53adb461cf872c0827bb97cf3959
DaCodeManiaK <jla.webprojet@gmail.com>
2022-03-22 16:09:33 +0100

    Test if an attribute exists in Room model

--- test/room.model.spec.ts
+++ test/room.model.spec.ts
@@ -1,8 +1,14 @@
 import { RoomModel } from './../src/models/room.model';
 
 describe('Testing room.model.ts', () => {
+    const room: RoomModel = new RoomModel();
+
     test('Got an instance of RoomModel', () => {
-        const room: RoomModel = new RoomModel();
         expect(room).toBeInstanceOf(RoomModel);
+    });
+
+    test(`Room must have a 'roomNumber' attribute`, () => {
+        const hasProperty: boolean = room.hasOwnProperty('roomNumber');
+        expect(hasProperty).toBeTruthy();
     })
 });

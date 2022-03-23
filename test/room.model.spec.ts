71e3dabc4882cbdb4e709e1077d58c543ff2025d
DaCodeManiaK <jla.webprojet@gmail.com>
2022-03-23 09:46:02 +0100

    Test room price getter and setter

--- test/room.model.spec.ts
+++ test/room.model.spec.ts
@@ -20,5 +20,10 @@ describe('Testing room.model.ts', () => {
     test(`Room number must be '10' after setting`, () => {
         room.setRoomNumber(10);
         expect(room.getRoomNumber()).toBe(10);
-    })
+    });
+
+    test(`Room price must be '45'`, () => {
+        room.setRoomPrice(45);
+        expect(room.getRoomPrice()).toBe(45);
+    });
 });

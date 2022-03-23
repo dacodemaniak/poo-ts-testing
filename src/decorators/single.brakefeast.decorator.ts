f4cf752b4ba51c26616dd9963bf9cd9f8ca610b2
DaCodeManiaK <jla.webprojet@gmail.com>
2022-03-23 14:16:52 +0100

    Update decorator to add the correct total

--- src/decorators/single.brakefeast.decorator.ts
+++ src/decorators/single.brakefeast.decorator.ts
@@ -1,7 +1,9 @@
+import { BrakefeastModel } from "../models/brakefeast.model";
 import { PriceDecorator } from "./price.decorator";
 
 export class SingleBrakefeastDecorator extends PriceDecorator {
     public total(): number {
-        return super.total();
+        const brakefeast: BrakefeastModel = new BrakefeastModel();
+        return brakefeast.total() + super.total();
     }
 }
